e6288ad798bbe3f7075a6edcdccac64497e45f64
timdecena <anthonycdecena@gmail.com>
2025-03-23 16:18:41 +0800

    backend-recreation(empty)

---
+++ decenabeldadsaceda/src/main/java/com/whiskerwatch/decenabeldadsaceda/DecenabeldadsacedaApplication.java
@@ -0,0 +1,13 @@
+package com.whiskerwatch.decenabeldadsaceda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DecenabeldadsacedaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DecenabeldadsacedaApplication.class, args);
+	}
+
+}
---
+++ decenabeldadsaceda/src/test/java/com/whiskerwatch/decenabeldadsaceda/DecenabeldadsacedaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.whiskerwatch.decenabeldadsaceda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DecenabeldadsacedaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
