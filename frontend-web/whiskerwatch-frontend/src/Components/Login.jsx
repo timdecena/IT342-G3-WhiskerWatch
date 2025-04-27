c00e7d9201403b92248f9f49f9fff690c58d0030
tyronebeldad123 <tyronebeldad123@gmail.com>
2025-04-27 22:45:36 +0800

    Update Login.jsx

--- frontend-web/whiskerwatch-frontend/src/Components/Login.jsx
+++ frontend-web/whiskerwatch-frontend/src/Components/Login.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import '../assets/login.css';
+import '../assets/Login.css';
 
 function Login({ setIsAuthenticated }) {
   const [email, setEmail] = useState('');
