194fc85c9adfc88fbd16ddb500376a47a4c9f04d
timdecena <anthonycdecena@gmail.com>
2025-04-30 19:32:58 +0800

    Copying of codes from main
    Copied code from main's Security config. applicaiton.properties, SecurityConfig, Register.jsx, added cors in WhiskerwatchApp.

--- frontend-web/whiskerwatch/src/main/java/edu/cit/whiskerwatch/WhiskerwatchApplication.java
+++ frontend-web/whiskerwatch/src/main/java/edu/cit/whiskerwatch/WhiskerwatchApplication.java
@@ -2,6 +2,9 @@ package edu.cit.whiskerwatch;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class WhiskerwatchApplication {
@@ -9,5 +12,20 @@ public class WhiskerwatchApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(WhiskerwatchApplication.class, args);
 	}
+
+	/*
+	 * @Bean
+	 * public WebMvcConfigurer corsConfigurer() {
+	 * return new WebMvcConfigurer() {
+	 * 
+	 * @Override
+	 * public void addCorsMappings(CorsRegistry registry) {
+	 * registry.addMapping("/**")
+	 * .allowedOrigins("http://ec2-35-168-15-40.compute-1.amazonaws.com")
+	 * .allowedMethods("*");
+	 * }
+	 * };
+	 * } -Copied from deployed main branch
+	 */
 
 }
